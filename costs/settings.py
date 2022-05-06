b17833584139fedd91a8f4b4be8e01581b2d5ce7
gvgeorge <gvgeorge@mail.ru>
2022-05-06 20:41:43 +0300

    changes to settings file

--- costs/settings.py
+++ costs/settings.py
@@ -1,7 +1,12 @@
 from pydantic import BaseSettings
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
 
 
 class Settings(BaseSettings):
+
     server_host: str = '0.0.0.0'
     server_port: int = 5555
     database_url: str = 'sqlite:///./database.sqlite3'
@@ -11,7 +16,8 @@ class Settings(BaseSettings):
     jwt_expiration: int = 3600
 
     class Config:
-        env_file = '.env'
+        env_prefix = ''
+        env_file = Path.joinpath(BASE_DIR, 'setenv')
         env_file_encoding = 'utf-8'
 
 
